1cf6f04952322f47a1244952071984df578ed310
Timothée Fleury <timothee@fromsmash.com>
2018-06-07 14:11:24 +0200

    Start date filter, translate filter, size filter

---
+++ index.js
@@ -0,0 +1,6 @@
+/*const TemplateEngine = require('./lib/templateEngine.js');
+const TemplateEngine = require('./lib/templateEngine.js');
+const TemplateEngine = require('./lib/templateEngine.js');
+const TemplateEngine = require('./lib/templateEngine.js');*/
+
+module.exports = {};
