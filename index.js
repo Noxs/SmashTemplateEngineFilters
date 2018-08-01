81285fede597dbe392833cf704742df22222d5ce
Félix Darraud <felix.darraud@gmail.com>
2018-08-01 12:49:18 +0200

    Fix index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const DateFilter = require('./lib/date/date.js');
 const SizeFilter = require('./lib/size/size.js');
-const TranslateFilter = require('./lib/size/translator.js');
+const TranslateFilter = require('./lib/translator/translator.js');
 
 
 module.exports = {Date : DateFilter, Size: SizeFilter, Translate: TranslateFilter};
