5e57304ad753531ef803188529457cecac77a5c6
Félix Darraud <felix.darraud@gmail.com>
2018-08-03 17:18:43 +0200

    Add plural and ucFirst filters

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const DateFilter = require('./lib/date/date.js');
 const SizeFilter = require('./lib/size/size.js');
 const TranslateFilter = require('./lib/translator/translator.js');
+const UcFirstFilter = require('./lib/ucFirst/ucFirst.js');
+const PluralFilter = require('./lib/plural/plural.js');
 
 
-module.exports = {Date : DateFilter, Size: SizeFilter, Translate: TranslateFilter};
+module.exports = {
+    Date: DateFilter,
+    Size: SizeFilter,
+    Translate: TranslateFilter,
+    UcFirst: UcFirstFilter,
+    Plural: PluralFilter
+};
