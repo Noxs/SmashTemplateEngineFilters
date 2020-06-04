1dea11d2efb489049a15ba53b81a59b5e59e03fe
Timothée Fleury <timothee@fromsmash.com>
2020-06-04 17:38:42 +0200

    Add slice filter

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Date = require('./lib/date/date.js');
 const Size = require('./lib/size/size.js');
+const Slice = require('./lib/slice/slice.js');
 const Translator = require('./lib/translator/translator.js');
 const ToUpperCase = require('./lib/toUpperCase/toUpperCase.js');
 const ToLowerCase = require('./lib/toLowerCase/toLowerCase.js');
@@ -11,15 +12,16 @@ const Nl2br = require('./lib/nl2br/nl2br.js');
 
 
 module.exports = {
-    Date: Date,
-    Size: Size,
+    Date,
+    Size,
     Translate: Translator,
-    Translator: Translator,
-    ToUpperCase: ToUpperCase,
-    ToLowerCase: ToLowerCase,
-    Plural: Plural,
-    Join: Join,
-    ForbiddenJsKeyword: ForbiddenJsKeyword,
-    Base64: Base64,
-    Nl2br: Nl2br,
+    Translator,
+    ToUpperCase,
+    ToLowerCase,
+    Plural,
+    Join,
+    ForbiddenJsKeyword,
+    Base64,
+    Nl2br,
+    Slice,
 };
